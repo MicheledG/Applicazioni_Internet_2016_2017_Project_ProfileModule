b5a02bf228511d12bb5e403b31505d1b7b96e487
MicheledG <chiefzephyr@gmail.com>
2017-06-10 10:13:45 +0200

    initial commit

---
+++ src/main/java/it/polito/ai/profilemodule/ProfileModuleApplication.java
@@ -0,0 +1,12 @@
+package it.polito.ai.profilemodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfileModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfileModuleApplication.class, args);
+	}
+}
